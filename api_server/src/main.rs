824118a89c649f782601c3e465b5b89f366bd3eb
Matt Burke <spraints@gmail.com>
2020-11-30 15:30:29 -0500

    remove database

--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -3,23 +3,11 @@
 #[macro_use]
 extern crate rocket;
 
-#[macro_use]
-extern crate rocket_contrib;
-
-// #[macro_use]
-extern crate diesel;
-
-mod schema;
-// mod models;
-
 use rocket::request::Request;
-use rocket_contrib::databases::diesel::PgConnection;
 
 #[get("/")]
-fn index(_db_conn: RustyDbConn) -> &'static str {
-    // Rocket uses the RustyDbConn request guard to provide us with a database
-    // connection from a managed pool.
-    "Hello, from Rust! (with a database connection!)"
+fn index() -> &'static str {
+    "Hello, from Rust!"
 }
 
 #[catch(503)]
@@ -27,12 +15,8 @@ fn service_not_available(_req: &Request) -> &'static str {
     "Service is not available. (Is the database up?)"
 }
 
-#[database("rustydb")]
-pub struct RustyDbConn(PgConnection);
-
 fn main() {
     rocket::ignite()
-        .attach(RustyDbConn::fairing())
         .register(catchers![service_not_available])
         .mount("/api", routes![index])
         .launch();
