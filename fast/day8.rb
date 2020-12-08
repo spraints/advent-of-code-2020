750d7e65ef6aa2bff18c95967f954179ee88e22b
Matt Burke <spraints@gmail.com>
2020-12-08 00:31:14 -0500

    rm debug puts

--- fast/day8.rb
+++ fast/day8.rb
@@ -34,7 +34,6 @@ def run2(prog, pc: 0, acc: 0, seen: {}, mutate: true)
   seen = seen.merge(pc => true)
   inst, val = prog[pc]
   return acc if inst.nil?
-  puts "pc=#{pc} #{inst} #{val}"
   case inst
   when 'acc'
     return run2 prog,
@@ -45,14 +44,12 @@ def run2(prog, pc: 0, acc: 0, seen: {}, mutate: true)
   when 'jmp'
     if mutate
       begin
-        puts "jmp => nop"
         return run2 prog,
           pc: pc + 1,
           acc: acc,
           seen: seen,
           mutate: false
       rescue LoopDetected
-        puts "BACKTRACK TO pc=#{pc}"
         # fall through
       end
     end
@@ -64,14 +61,12 @@ def run2(prog, pc: 0, acc: 0, seen: {}, mutate: true)
   when 'nop'
     if mutate
       begin
-        puts "nop => jmp"
         return run2 prog,
           pc: pc + val,
           acc: acc,
           seen: seen,
           mutate: false
       rescue LoopDetected
-        puts "BACKTRACK TO pc=#{pc}"
         # fall through
       end
     end
