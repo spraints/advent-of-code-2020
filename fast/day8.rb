de974ff6a513c3c56416d6c4d03daa3f92a8ffb3
Matt Burke <spraints@gmail.com>
2020-12-08 00:26:41 -0500

    day8 part2

--- fast/day8.rb
+++ fast/day8.rb
@@ -25,6 +25,66 @@ def main(input)
     break if seen[pc]
   end
   puts "part 1: #{acc}"
+  acc = run2(prog)
+  puts "part 2: #{acc}"
+end
+
+def run2(prog, pc: 0, acc: 0, seen: {}, mutate: true)
+  raise LoopDetected if seen[pc]
+  seen = seen.merge(pc => true)
+  inst, val = prog[pc]
+  return acc if inst.nil?
+  puts "pc=#{pc} #{inst} #{val}"
+  case inst
+  when 'acc'
+    return run2 prog,
+      pc: pc + 1,
+      acc: acc + val,
+      seen: seen,
+      mutate: mutate
+  when 'jmp'
+    if mutate
+      begin
+        puts "jmp => nop"
+        return run2 prog,
+          pc: pc + 1,
+          acc: acc,
+          seen: seen,
+          mutate: false
+      rescue LoopDetected
+        puts "BACKTRACK TO pc=#{pc}"
+        # fall through
+      end
+    end
+    return run2 prog,
+      pc: pc + val,
+      acc: acc,
+      seen: seen,
+      mutate: mutate
+  when 'nop'
+    if mutate
+      begin
+        puts "nop => jmp"
+        return run2 prog,
+          pc: pc + val,
+          acc: acc,
+          seen: seen,
+          mutate: false
+      rescue LoopDetected
+        puts "BACKTRACK TO pc=#{pc}"
+        # fall through
+      end
+    end
+    return run2 prog,
+      pc: pc + 1,
+      acc: acc,
+      seen: seen,
+      mutate: mutate
+  end
+  raise "unexpected #{inst.inspect} pc=#{pc}"
+end
+
+class LoopDetected < StandardError
 end
 
 main($stdin.read)
