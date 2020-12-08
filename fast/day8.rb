66946e215603e813711a17077ebee2e7dd767e40
Matt Burke <spraints@gmail.com>
2020-12-08 10:52:17 -0500

    find the missing link

--- fast/day8.rb
+++ fast/day8.rb
@@ -5,15 +5,55 @@ def main(input)
     sign = $2
     val = $3.to_i
     val = -val if sign == '-'
-    {inst: inst, val: val, ok: [], nok: [], step: []}.freeze
-  } + [{ok: [], nok: [], step: [:EOF]}]).freeze
+    {inst: inst, val: val}
+  })
+  puts "part 1: #{run(prog)} (expect 1832)"
+
+  prog << {inst: :END}
+  prog.each_with_index do |x, i|
+    inst = x[:inst]
+    val = x[:val]
+    case inst
+    when 'jmp'
+      (prog[i + val][:from] ||= []) << i
+      (prog[i + 1][:mfrom] ||= []) << i
+    when 'nop'
+      (prog[i + 1][:from] ||= []) << i
+      (prog[i + val][:mfrom] ||= []) << i
+    when 'acc'
+      (prog[i + 1][:from] ||= []) << i
+    end
+  end
+
+  end_reachable = [prog.size - 1]
+  broken = true
+  while broken && pc = end_reachable.shift
+    end_reachable += (prog[pc][:from] || [])
+    (prog[pc][:mfrom] || []).each do |i|
+      if prog[i][:reachable]
+        p broken: prog[i], i: i
+        prog[i][:inst] = 
+          if prog[i][:inst] == 'jmp'
+            'nop'
+          else
+            'jmp'
+          end
+        broken = false
+        break
+      end
+    end
+  end
+
+  prog.each { |x| x.delete :reachable }
+  puts "part 2: #{run(prog)} (expect 662)"
+end
+
+def run(prog)
   acc = 0
   pc = 0
-  steps = 0
   loop do
     x = prog[pc]
-    x[:step] << steps
-    steps += 1
+    x[:reachable] = true
     case x.fetch(:inst)
     when 'acc'
       acc += x.fetch(:val)
@@ -22,86 +62,13 @@ def main(input)
       pc += 1
     when 'jmp'
       pc += x.fetch(:val)
+    when :END
+      return [acc, true]
     end
-    break unless prog[pc][:step].empty?
-  end
-  puts "part 1: #{acc} (expect 1832)"
-  acc = run2(prog)
-  puts "part 2: #{acc} (expect 662)"
-
-  prog.each_with_index do |x, i|
-    inst = c.fetch(:inst)
-    val = c.fetch(:val)
-    case inst
-    when 'acc'
-      prog[i+1][:ok] << i
-    when 'jmp'
-      prog[i+val][:ok] << i
-      prog[i+1][:nok] << i
-    when 'nop'
-      prog[i+1][:ok] << i
-      prog[i+val][:nok] << i
+    if prog[pc][:reachable]
+      return [acc, false]
     end
   end
-  pc = prog.size - 1
-  loop do
-    # https://www.geeksforgeeks.org/minimum-cost-of-simple-path-between-two-nodes-in-a-directed-and-weighted-graph/ ?
-  end
-end
-
-def run2(prog, pc: 0, acc: 0, seen: {}, mutate: true)
-  raise LoopDetected if seen[pc]
-  seen = seen.merge(pc => true)
-  x = prog[pc]
-  inst = x[:inst]
-  val = x[:val]
-  return acc if inst.nil?
-  case inst
-  when 'acc'
-    return run2 prog,
-      pc: pc + 1,
-      acc: acc + val,
-      seen: seen,
-      mutate: mutate
-  when 'jmp'
-    if mutate
-      begin
-        return run2 prog,
-          pc: pc + 1,
-          acc: acc,
-          seen: seen,
-          mutate: false
-      rescue LoopDetected
-        # fall through
-      end
-    end
-    return run2 prog,
-      pc: pc + val,
-      acc: acc,
-      seen: seen,
-      mutate: mutate
-  when 'nop'
-    if mutate
-      begin
-        return run2 prog,
-          pc: pc + val,
-          acc: acc,
-          seen: seen,
-          mutate: false
-      rescue LoopDetected
-        # fall through
-      end
-    end
-    return run2 prog,
-      pc: pc + 1,
-      acc: acc,
-      seen: seen,
-      mutate: mutate
-  end
-  raise "unexpected #{inst.inspect} pc=#{pc}"
-end
-
-class LoopDetected < StandardError
 end
 
 main($stdin.read)
