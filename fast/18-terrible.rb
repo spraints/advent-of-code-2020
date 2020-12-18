042c36fdadbfaa1bd025c50954f676649d136257
Matt Burke <spraints@gmail.com>
2020-12-18 08:34:40 -0500

    one pass

--- fast/18-terrible.rb
+++ fast/18-terrible.rb
@@ -1,21 +1,13 @@
 class Integer
   alias_method :add, :+
-  alias_method :mul, :*
+  alias_method :+, :*
+  alias_method :*, :add
+  alias_method :-, :add
 end
 lines = $stdin.readlines
-
-class Integer
-  alias_method :+, :add
-  alias_method :-, :mul
-end
 p lines.map { |line|
-    eval(line.tr("+*", "+-"))
+    eval(line.tr("+*", "-+"))
 }.sum
-
-class Integer
-  alias_method :+, :mul
-  alias_method :*, :add
-end
 p lines.map { |line|
     eval(line.tr("+*", "*+"))
 }.sum
