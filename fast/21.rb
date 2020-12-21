ccc6b3b6a9ca213ee47030ec870eead48b1ef89d
Matt Burke <spraints@gmail.com>
2020-12-21 00:21:16 -0500

    day 21: the ugly

---
+++ fast/21.rb
@@ -0,0 +1,51 @@
+require_relative "./lib"
+require "set"
+
+def main(input)
+  bm "parse"
+
+  bm "part 1"
+
+  all_ingredients = []
+  possible_allergens = {}
+  input.lines.each do |line|
+    line =~ /(.*) \(contains (.*)\)/ or raise line.inspect
+    ingredients = $1.split(" ")
+    allergens = $2.split(", ")
+    all_ingredients += ingredients
+    allergens.each do |a|
+      if y = possible_allergens[a]
+        possible_allergens[a] = y & ingredients
+      else
+        possible_allergens[a] = ingredients
+      end
+    end
+  end
+
+  bad = {}
+  until possible_allergens.empty?
+    np = {}
+    possible_allergens.sort_by { |a, b| b.size }.each do |a, i|
+      j = i - bad.keys
+      if j.size == 0
+        raise "impossible #{[a, i, j, bad].inspect}"
+      elsif j.size == 1
+        bad[j[0]] = a
+      else
+        np[a] = j
+      end
+    end
+    possible_allergens = np
+  end
+
+  puts "part 1: #{(all_ingredients - bad.keys).size}"
+
+  bm "part 2"
+
+  puts "part 2: #{bad.sort_by(&:last).map(&:first).join(",")}"
+
+ensure
+  bm_done
+end
+
+main($stdin.read)
