a6529f1166b776ddea435e3c424dde65877ea14a
Matt Burke <spraints@gmail.com>
2020-12-25 00:58:20 -0500

    clean up day 25

--- fast/25.rb
+++ fast/25.rb
@@ -9,36 +9,18 @@ def main(input)
 
   pk1, pk2 = input.lines.map(&:to_i)
 
-  ls1 = bfpk(pk1)
   ls2 = bfpk(pk2)
-  p [ls1, ls2]
 
   enc = transform subject: pk1, ls: ls2
-  #enc = transform subject: pk2, ls: ls1
 
   p1done enc
 
-  #card_subj = 7
-  #door_subj = 7
+  # 7^ls1 = pk1 mod 20201227
+  # 7^ls2 = pk2 mod 20201227
+  # pk1^ls2 = enc mod 20201227
+  # pk2^ls1 = enc mod 20201227
   #
-  #card_ls = 8
-  #door_ls = 11
-  #
-  # pk = 7^ls mod 20201227
-  #card_pk = transform subject: card_subj, ls: card_ls
-  #door_pk = transform subject: door_subj, ls: door_ls
-  #
-  #enc_key = transform subject: card_pk, ls: door_ls
-  #enc_key2 = transform subject: door_pk, ls: card_ls
-  #
-  #p1done \
-  #  card_pk: card_pk,
-  #  door_pk: door_pk,
-  #  enc_key: [enc_key, enc_key2]
-
-  # ---------------------------------
-
-  p2done 0
+  # (7^ls1)^ls2 = enc mod 20201227
 
 ensure
   bm_done
